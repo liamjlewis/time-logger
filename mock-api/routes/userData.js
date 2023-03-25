4c11130d660fe7adb0d5f19b4e5b81f3956f2072
Liam Lewis <liamjlewis@gmail.com>
2023-03-25 15:09:10 +0000

    API expects a user ID in a POST request for user data

--- mock-api/routes/userData.js
+++ mock-api/routes/userData.js
@@ -4,8 +4,17 @@ var router = express.Router();
 let mockUsers = require('../dummy-data/user-data.json');
 
 /* GET users listing. */
-router.get('/', async function(req, res, next) {
-  res.json( mockUsers )
+router.post('/', async function(req, res, next) {
+  if (!req.body.id) {
+    res.status(400).send('user id is required')
+  } else {
+    const userData = mockUsers[req.body.id];
+    if (!!userData) {
+      res.json( userData )
+    } else {
+      res.status(404).send('user not found')
+    }
+  }
 });
 
 module.exports = router;
